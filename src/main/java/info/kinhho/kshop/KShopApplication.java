280d1d2f595b04352e6809bed17001f884ca6251
Kinh Ho <hongockinh@outlook.com>
2024-11-01 13:54:54 +0700

    Initialize source code

---
+++ src/main/java/info/kinhho/kshop/KShopApplication.java
@@ -0,0 +1,13 @@
+package info.kinhho.kshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/kinhho/kshop/KShopApplicationTests.java
@@ -0,0 +1,13 @@
+package info.kinhho.kshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
